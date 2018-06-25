9c9aca14afa8a1097f126bfcebe53d5356888222
lucash-dev <lucash.dev@gmail.com>
2018-06-24 18:11:41 -0700

    Reduced number of validations in `tx_validationcache_tests` to keep the run time reasonable.
    Following a suggestion in the comments, changed `ValidateCheckInputsForAllFlags` from testing all possible flag combinations to testing a random subset. Also created a new enum constant for the highest flag, so that this test doesn’t keep testing an incomplete subset in case a new flag is added.

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -114,6 +114,11 @@ enum
     // Making OP_CODESEPARATOR and FindAndDelete fail any non-segwit scripts
     //
     SCRIPT_VERIFY_CONST_SCRIPTCODE = (1U << 16),
+
+    // Constants to point to the highest flag in use. Add new flags above this line.
+    //
+    HIGHEST_FLAG_PLUS_ONE,
+    HIGHEST_FLAG = HIGHEST_FLAG_PLUS_ONE - 1
 };
 
 bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned int flags, ScriptError* serror);
